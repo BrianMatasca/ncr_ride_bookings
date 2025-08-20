ed457a9c014b61134e2e31d4c6aa67313b1b519c
Juan Plaza <jufeplasa@hotmail.com>
2025-08-20 00:18:39 -0500

    Optio of select csv file and descompose transform method

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from util import ETL;
 
 def main():
-    etl_process = ETL("source_data", "destination_data")
+    etl_process = ETL()
+    new_df = etl_process.etlProcess()
+    print(new_df.head())
+    print(new_df.info())
 
 if __name__ == "__main__":
     main()
